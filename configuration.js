2d02c3b33f3fa07768b6eeb315ace486ed81628b
juliette-ferlin <juliette.ferlin.doc@gmail.com>
2024-11-05 15:10:40 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,52 +1,30 @@
 const config = {
-  name: "Guillaume Levrier",
+  name: "Juliette Ferlin",
   description: {
     EN: {
-      short: `Guillaume Levrier is a political scientist working on biotechnology, currently a CNRS postdoc for the
-        <a href="https://cordis.europa.eu/project/id/951393" target="_blank">Nanobubbles ERC grant</a>. He is affiliated
-        to the <a href="http://umr-lisis.fr/membre/guillaume-levrier/" target="_blank">Laboratoire Interdisciplinaire
-          Sciences Innovations Sociétés (LISIS)</a>, and is a research associate to <a
-            href="https://www.sciencespo.fr/cevipof/en/researcher/guillaume-levrier.html"
-            target="_blank">CEVIPOF (Sciences Po)</a> and to the French National Library (BnF). `,
+      short: `Juliette Ferlin has been completing a doctorate in sociology since November 2024. She is affiliated
+        to the <a href="http://umr-lisis.fr/membre/" target="_blank">Laboratoire Interdisciplinaire
+          Sciences Innovations Sociétés (LISIS)</a> 
+          and is supervised by <a href="https://umr-lisis.fr/membre/francois-dedieu/" target="_blank"> 
+          François Dedieu 
+          and 
+          <a href="http://iris.ehess.fr/index.php?2984" target="_blank">Tristan Fournier.`,
       long: "",
     },
     FR: {
-      short: `Guillaume Levrier est un chercheur en sciences
-        politiques
-        spécialisé sur les biotechnologies, actuellement postdoc CNRS pour le <a
-          href="https://cordis.europa.eu/project/id/951393" target="_blank">projet
-          ERC Nanobubbles</a>. Il est affilié au <a
-            href="http://umr-lisis.fr/membre/guillaume-levrier/"
-            target="_blank">Laboratoire Interdisciplinaire
-          Sciences
-          Innovations Sociétés (LISIS)</a>, est associé au <a
-            href="https://www.sciencespo.fr/cevipof/fr/chercheur/guillaume-levrier.html"
-            target="_blank">CEVIPOF (Sciences Po)</a> et à la Bibliothèque nationale de
-        France (BnF).`,
+      short: `Juliette Ferlin réalise un doctorat en sociologie depuis Novembre 2024. 
+      Elle est affiliée au <a href="http://umr-lisis.fr/membre/" target="_blank">Laboratoire Interdisciplinaire
+          Sciences Innovations Sociétés (LISIS)</a> et est supervisée par 
+          <a href="https://umr-lisis.fr/membre/francois-dedieu/" target="_blank"> 
+          François Dedieu 
+          et 
+          <a href="http://iris.ehess.fr/index.php?2984" target="_blank">Tristan Fournier.`
       long: "",
     },
   },
   links: [
     {
-      label: "twitter",
-      value: "@g_levrier",
-      url: "https://twitter.com/g_levrier",
-    },
-    {
-      label: "google scholar",
+      label: "HAL",
       value: "profile",
-      url: "https://scholar.google.com/citations?user=IRiJFNMAAAAJ&hl=en",
+      url: "",
     },
-    {
-      label: "pubpeer",
-      value: "profile",
-      url: "https://pubpeer.com/search?q=guillaume+levrier",
-    },
-    {
-      label: "linked in",
-      value: "profile",
-      url: "https://www.linkedin.com/in/guillaume-levrier-phd-89156838/",
-    },
-  ],
-  email: { name: "guillaume.levrier", domain: "politique.science" },
-};
